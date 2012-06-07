5addeb69774fe126a88e061f6c9a08d2acb150c0
Otto Nascarella <otto@rhino.(none)>
2012-06-07 01:32:17 +0100

    changes in the code

--- jquery.textblur.js
+++ jquery.textblur.js
@@ -1,70 +1,77 @@
-/* 
- * JQuery TextBlur Filter
- * https://github.com/ottonascarella/TextBlur
- * Copyright (c) 2011 Otto Nascarella
- * licensed under the LGPL Version 3 license.
- * http://www.gnu.org/licenses/lgpl.html 
- */
-(function($) {
-    
-    $.cssHooks['textBlur'] = {
-        get: function(elem, computed, extra){
-        	if (!$.browser.msie) {
-        		var str = $(elem).css('text-shadow');
-	          if (str == 'none') {
-	            $(elem).css('text-shadow','rgb(0, 0, 0) 0px 0px 0px');
-	            return 0;
-	          }
-	          return str.split(' ')[5].split('px')[0];
-          } else {
-        		var str = $(elem).css('filter');
-	          if (str == 'none') {
-	            $(elem).css('filter','progid:DXImageTransform.Microsoft.Blur(pixelradius=0)');
-	            return 0;
-	          }
-	          return str.slice(str.indexOf('radius=')+7,-1);          
-          }
-        }, // end of get
-        set: function(elem, value) {
-        	if (!$.browser.msie) {
-	        	if (typeof $(elem).data('textShadow') == 'undefined') {
-							if (typeof $(elem).css('textShadow') == 'undefined')
-								$(elem).data('textShadow', '');
-							else
-								$(elem).data('textShadow', $(elem).css('textShadow'));
-						}
-						if (typeof $(elem).data('color') == 'undefined') $(elem).data('color', $(elem).css('color'));
-	
-						if (value != 0) {
-							$(elem).css({color: 'transparent', textShadow: '0px 0px ' + parseInt(value) + 'px ' + $(elem).data('color') }); 
-						}
-						else {
-							$(elem).css({color: $(elem).data('color'), textShadow: ''});
-							$(elem).removeData('color');
-							$(elem).removeData('textShadow'); 
-						}
-					} else {
-	        	if (typeof $(elem).data('filter') == 'undefined') {
-							if (typeof $(elem).css('filter') == 'undefined')
-								$(elem).data('filter', '');
-							else
-								$(elem).data('filter', $(elem).css('filter'));
-						}
-	
-						if (value != 0)
-							$(elem).css('filter', 'progid:DXImageTransform.Microsoft.Blur(pixelradius=' + parseInt(value) + ')');
-						else 
-							$(elem).css('filter', $(elem).data('filter'));
-							$(elem).removeData('filter');						
+/*
+* JQuery TextBlur Filter
+* https://github.com/ottonascarella/TextBlur
+* Copyright (c) 2011 Otto Nascarella
+* licensed under the LGPL Version 3 license.
+* http://www.gnu.org/licenses/lgpl.html
+*/
+;(function($) {
+	$.cssHooks['textBlur'] = {
+		get: (function() {
+			var func;
+			if (!$.browser.msie) {
+				func = function(elem, computed, extra) {
+					var str = $(elem).css('textShadow');
+					if (str === 'none' || str === "") {
+						$(elem).css('textShadow', 'none');
+						return 0;
 					}
-				} // end of set
-    }; // end of css hooks
-    
-    $.fx.step['textBlur'] = function(fx){
-        $.cssHooks['textBlur'].set(fx.elem, fx.now);
-    };
-    
+					return str.slice(str.lastIndexOf(" ")+1);
+				};
+			} else {
+				func = function(elem, computed, extra) {
+					var str = $(elem).css('filter');
+					if (str === 'none') {
+						$(elem).css('filter', 'progid:DXImageTransform.Microsoft.Blur(pixelradius=0)');
+						return 0;
+					}
+					return str.slice(str.lastIndexOf('radius=')+1, str.lastIndexOf(')'));
+				};
+			}
+			return func;
+		}()),
+		// end of get
+		set: (function() {
+			var func;
+			if (!$.browser.msie) {
+				func = function(elem, value) {
+					if (!$(elem).data().textShadow) {
+						if ($(elem).css('textShadow') === "none") $(elem).data('textShadow', 'none');
+						else $(elem).data('textShadow', $(elem).css('textShadow'));
+					}
+					
+					if (!$(elem).data().color) $(elem).data('color', $(elem).css('color'));
+
+					if (value !== 0) {
+						$(elem).css({
+							color: 'rgba(0,0,0,0)',
+							textShadow: '0px 0px ' + parseInt(value,10) + 'px ' + $(elem).data('color')
+						});
+					} else {
+						$(elem).css({
+							color: $(elem).data('color'),
+							textShadow: $(elem).data('textShadow')
+						});
+						$(elem).removeData('color').removeData('textShadow');
+					}
+				};
+			} else {
+				func = function(elem, value) {
+					if (!$(elem).data().filter) {
+						if ($(elem).css('filter') === 'none') $(elem).data('filter', '');
+						else $(elem).data('filter', $(elem).css('filter'));
+					}
+
+					if (value !== 0) $(elem).css('filter', 'progid:DXImageTransform.Microsoft.Blur(pixelradius=' + parseInt(value,10) + ')');
+					else $(elem).css('filter', $(elem).data('filter'));
+					$(elem).removeData('filter');
+				};
+			}
+		return func;
+		}()) // end of set
+	}; // end of css hooks
+	$.fx.step['textBlur'] = function(fx) {
+		$.cssHooks['textBlur'].set(fx.elem, fx.now);
+	};
+
 })(jQuery);
-
-
-
